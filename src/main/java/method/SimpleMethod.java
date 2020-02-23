1bcac8a0bd38b6f9890a0ddd7809d2bccded9e28
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-02-23 19:57:42 +0100

    Input and output arguments for OPC-UA-Skill are now build with Annotations and getter methods are not longer necessary

--- src/main/java/method/SimpleMethod.java
+++ src/main/java/method/SimpleMethod.java
@@ -1,53 +1,44 @@
 package method;
 
-import org.eclipse.milo.opcua.sdk.core.ValueRanks;
+import org.eclipse.milo.opcua.sdk.core.ValueRank;
 import org.eclipse.milo.opcua.sdk.server.api.methods.AbstractMethodInvocationHandler.InvocationContext;
-import org.eclipse.milo.opcua.stack.core.Identifiers;
 import org.eclipse.milo.opcua.stack.core.UaException;
-import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-import org.eclipse.milo.opcua.stack.core.types.structured.Argument;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import annotations.InputArgument;
+import annotations.InputArguments;
+import annotations.OpcUaMethod;
+import annotations.OutputArgument;
+import annotations.OutputArguments;
+import annotations.UaTypes;
 import methodRegistration.MethodRegistration;
 
 /**
  * @Component Indicates that annotated class is intended to be an OSGi
  *            component. <br>
- *            After becoming satisfied component is registered as a service under MethodRegistration
+ *            After becoming satisfied component is registered as a service
+ *            under MethodRegistration
  */
+@OpcUaMethod
 @Component
 public class SimpleMethod implements MethodRegistration {
-	
-	public static final Argument[] inputArguments = {
-			new Argument("1. Zahl", Identifiers.Double, ValueRanks.Scalar, null, new LocalizedText("1. Summand")),
-			new Argument("2. Zahl", Identifiers.Double, ValueRanks.Scalar, null, new LocalizedText("2. Summand")) };
-
-	public static final Argument outputArgument = new Argument("Ergebnis", Identifiers.Double, ValueRanks.Scalar, null,
-			new LocalizedText("Summe"));
 
 	private final Logger logger = LoggerFactory.getLogger(SimpleMethod.class);
-
-	@Override
-	public Argument[] getInputArguments() {
-		// TODO Auto-generated method stub
-		return inputArguments;
-	}
-
-	@Override
-	public Argument[] getOutputArguments() {
-		// TODO Auto-generated method stub
-		return new Argument[] { outputArgument };
-	}
 
 	/**
 	 * Method that turns input arguments into output argument
 	 */
 	@Override
+	@InputArguments({
+			@InputArgument(name = "1. Zahl", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "1. Summand"),
+			@InputArgument(name = "2. Zahl", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "2. Summand") })
+	@OutputArguments({
+			@OutputArgument(name = "Ergebnis", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "Summe") })
 	public Variant[] invoke(InvocationContext context, Variant[] inputValues) throws UaException {
 		logger.debug("Invoking simpleMethod of objectId={}", context.getObjectId());
 
@@ -60,11 +51,11 @@ public class SimpleMethod implements MethodRegistration {
 
 	@Activate
 	public void activate() {
-		logger.info("Methode wird aktiviert.");
+		logger.info("Methode zum Addieren wird aktiviert.");
 	}
 
 	@Deactivate
 	public void deactivate() {
-		logger.info("Methode wird deaktiviert.");
+		logger.info("Methode zum Addieren wird deaktiviert.");
 	}
 }
