fb47951ce221c699c7e770c8ed6f1acb673f472c
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-03-18 17:18:01 +0100

    Interface no longer specifies anything, only for tracking skills

--- OPC-UA-Skill-Registration/src/main/java/opcuaSkillRegistration/OPCUASkillRegistration.java
+++ OPC-UA-Skill-Registration/src/main/java/opcuaSkillRegistration/OPCUASkillRegistration.java
@@ -1,11 +1,5 @@
 package opcuaSkillRegistration;
 
-import org.eclipse.milo.opcua.sdk.server.api.methods.AbstractMethodInvocationHandler.InvocationContext;
-import org.eclipse.milo.opcua.stack.core.UaException;
-import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-
 public interface OPCUASkillRegistration {
-	
-	public Variant[] invoke(InvocationContext context, Variant[] inputValues) throws UaException;
 
 }
