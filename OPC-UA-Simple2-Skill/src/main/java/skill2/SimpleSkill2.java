85d5d2b5424d82db37c464c567bc32276332d05d
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-03-18 17:19:07 +0100

    Skill is defined by actions for corresponding states (start, stop...).  Action has parameter which are the input arguments for the skill and a return type which is the output argument.

--- OPC-UA-Simple2-Skill/src/main/java/skill2/SimpleSkill2.java
+++ OPC-UA-Simple2-Skill/src/main/java/skill2/SimpleSkill2.java
@@ -1,22 +1,18 @@
 package skill2;
 
-import org.eclipse.milo.opcua.sdk.core.ValueRank;
-import org.eclipse.milo.opcua.sdk.server.api.methods.AbstractMethodInvocationHandler.InvocationContext;
-import org.eclipse.milo.opcua.stack.core.UaException;
-import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import annotations.InputArgument;
-import annotations.InputArguments;
-import annotations.OpcUaSkill;
-import annotations.OutputArgument;
-import annotations.OutputArguments;
-import annotations.UaTypes;
+import annotations.SkillParameter;
+import annotations.SkillReturn;
+import annotations.Starting;
+import annotations.Stopping;
 import opcuaSkillRegistration.OPCUASkillRegistration;
+import statemachine.StateMachine;
+import statemachine.StateMachineBuilder;
 
 /**
  * @Component Indicates that annotated class is intended to be an OSGi
@@ -24,34 +20,28 @@ import opcuaSkillRegistration.OPCUASkillRegistration;
  *            After becoming satisfied component is registered as a service
  *            under OPCUASkillRegistration
  */
-@OpcUaSkill
 @Component
 public class SimpleSkill2 implements OPCUASkillRegistration {
 
 	private final Logger logger = LoggerFactory.getLogger(SimpleSkill2.class);
 
-	/**
-	 * Method that turns input arguments into output argument
-	 */
-	@Override
-	@InputArguments({
-			@InputArgument(name = "1. Zahl", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "Minuend"),
-			@InputArgument(name = "2. Zahl", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "Substrahend") })
-	@OutputArguments({
-			@OutputArgument(name = "Ergebnis", dataType = UaTypes.Double, valueRank = ValueRank.Scalar, arrayDimensions = "", description = "Differenz") })
-	public Variant[] invoke(InvocationContext context, Variant[] inputValues) throws UaException {
-		logger.debug("Invoking simpleMethod of objectId={}", context.getObjectId());
+	@Starting
+	public @SkillReturn(name = "Ergebnis", description = "Differenz") double start(
+			@SkillParameter(name = "1. Zahl", description = "Minuend") double a,
+			@SkillParameter(name = "2. Zahl", description = "Substrahend") double b) {
 
-		double firstNumber = (double) inputValues[0].getValue();
-		double secondNumber = (double) inputValues[1].getValue();
-		double sum = firstNumber - secondNumber;
+		return a - b;
+	}
 
-		return new Variant[] { new Variant(sum) };
+	@Stopping
+	public String stop(String stop) {
+
+		return stop;
 	}
 
 	@Activate
 	public void activate() {
-		logger.info("OPC-UA-Skill zum Subtrahieren wird aktiviert.");
+		logger.info("OPC-UA-Skill zum Subtrahieren wird aktiviert."); 
 	}
 
 	@Deactivate
