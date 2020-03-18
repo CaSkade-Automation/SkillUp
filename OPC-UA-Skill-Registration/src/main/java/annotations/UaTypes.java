489cdc8cd8e8ff6dc9a29fc7777509327a3682d0
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-03-18 17:17:16 +0100

    Added String to enumeration of UaTypes for NodeId and method to check if name of parameter exists in enumeration and gives this value or if it not exists null

--- OPC-UA-Skill-Registration/src/main/java/annotations/UaTypes.java
+++ OPC-UA-Skill-Registration/src/main/java/annotations/UaTypes.java
@@ -1,8 +1,10 @@
 package annotations;
 
+import com.google.common.base.Enums;
+
 public enum UaTypes {
 	Boolean(1), SByte2(2), Byte(3), Int16(4), UInt16(5), Int32(6), UInt32(7), Int64(8), UInt64(9), Float(10),
-	Double(11);
+	Double(11), String(12);
 
 	private final int key;
 
@@ -12,5 +14,9 @@ public enum UaTypes {
 
 	public int getKey() {
 		return key;
+	}
+	
+	public static UaTypes getIfPresent(String name) {
+		return Enums.getIfPresent(UaTypes.class, name).orNull(); 
 	}
 }
