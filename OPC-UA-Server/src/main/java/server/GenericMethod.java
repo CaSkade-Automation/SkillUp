2873cb9db3817f3abb824c2e12c43ed8c4b94acd
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-03-19 16:33:16 +0100

    GenericMethod needs only node, stateMachine and transitionType. Input and Output -arguments are null. For the corresponding transitionType the transition of the stateMachine is executed.

--- OPC-UA-Server/src/main/java/server/GenericMethod.java
+++ OPC-UA-Server/src/main/java/server/GenericMethod.java
@@ -1,82 +1,76 @@
 package server;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.eclipse.milo.opcua.sdk.server.api.methods.AbstractMethodInvocationHandler;
 import org.eclipse.milo.opcua.sdk.server.nodes.UaMethodNode;
 import org.eclipse.milo.opcua.stack.core.UaException;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 import org.eclipse.milo.opcua.stack.core.types.structured.Argument;
-import opcuaSkillRegistration.OPCUASkillRegistration;
 import statemachine.StateMachine;
 
 /**
  * Class is used to reflect the skill to be added to the server <br>
  */
 public class GenericMethod extends AbstractMethodInvocationHandler {
-
-	OPCUASkillRegistration skillRegistration; 
-	Method method;  
-	Argument[] inputArguments; 
-	Argument[] outputArguments; 
-	
+ 
 	StateMachine stateMachine; 
-
-//	public GenericMethod(UaMethodNode node, OPCUASkillRegistration skillRegistration, Method method, Argument[] inputArguments, Argument[] outputArguments) {
-//		super(node);
-//		this.skillRegistration = skillRegistration; 
-//		this.method = method;
-//		this.inputArguments = inputArguments; 
-//		this.outputArguments = outputArguments; 
-//		// TODO Auto-generated constructor stub
-//	}
+	String transitionType; 
 	
-	public GenericMethod(UaMethodNode node, StateMachine stateMachine, Method method) {
+	public GenericMethod(UaMethodNode node, StateMachine stateMachine, String transitionType) {
 		super(node); 
-		this.method = method; 
-		this.stateMachine = stateMachine; 
+		this.stateMachine = stateMachine;  
+		this.transitionType = transitionType; 
 	}
 
 	@Override
 	public Argument[] getInputArguments() {
 		// TODO Auto-generated method stub
-		return inputArguments;
+		//important!! new Argument[0], because AbstractMethodInvocationHandler checks in line 63 if length of inputValues == length of Argument
+		//and by returning null it occurs an error. Then AbstractMethodInvocationHandler sets inputValues to new Variant[0] if inputValues are null  
+		return new Argument[0];
 	}
 
 	@Override
 	public Argument[] getOutputArguments() {
 		// TODO Auto-generated method stub
-		return outputArguments;
+		return new Argument[0];
 	}
 
 	@Override
 	protected Variant[] invoke(InvocationContext invocationContext, Variant[] inputs) throws UaException {
 		// TODO Auto-generated method stub
 		
-		if(method.getName().equals("start")) {
+		if(transitionType.equals("Start")) {
 			stateMachine.start(); 
 		}
+		else if(transitionType.equals("Stop")) {
+			stateMachine.stop();
+		}
+		else if(transitionType.equals("Clear")) {
+			stateMachine.clear(); 
+		}
+		else if(transitionType.equals("Abort")) {
+			stateMachine.abort(); 
+		}
+		else if(transitionType.equals("Hold")) {
+			stateMachine.hold();
+		}
+		else if(transitionType.equals("Reset")) {
+			stateMachine.reset();
+		}
+		else if(transitionType.equals("Suspend")) {
+			stateMachine.suspend();
+		}
+		else if(transitionType.equals("Unhold")) {
+			stateMachine.unhold();
+		}
+		else if(transitionType.equals("Unsuspsend")) {
+			stateMachine.unsuspend();
+		}
 		
-		return null; 
+		//stateMachine.invokeTransition(transitionType); 
 		
-		
-		
-//		List<Object> inputValues = new ArrayList<>(); 
-//		for (Variant input : inputs) {
-//			inputValues.add(input.getValue());
-//		}
-//		try {
-//			Object result = this.method.invoke(skillRegistration, inputValues.toArray());
-//			
-//			return new Variant[] { new Variant(result) };
-//			
-//		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			return null;
-//		}
+		return null;  
 	}
 }
