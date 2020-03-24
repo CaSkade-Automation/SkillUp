495272fdc8c85160704a014e6677f39206314333
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-03-24 18:59:07 +0100

    ActionGenerator doesn't get object of interface anymore, but an object of the skill (instance of factory component).

--- ActionGenerator/src/main/java/actionGenerator/ActionGenerator.java
+++ ActionGenerator/src/main/java/actionGenerator/ActionGenerator.java
@@ -5,24 +5,23 @@ import java.lang.reflect.Method;
 import org.osgi.service.component.annotations.Component;
 
 import annotations.States;
-import opcuaSkillRegistrationInterface.OPCUASkillRegistrationInterface;
 import statemachine.StateMachine;
 import statemachine.StateMachineBuilder;
 
 @Component(immediate=true, service=ActionGenerator.class)
 public class ActionGenerator {
 	
-	public StateMachine generateAction(OPCUASkillRegistrationInterface skillRegistration) {
+	public StateMachine generateAction(Object skill) {
 
 		StateMachineBuilder stateMachineBuilder = new StateMachineBuilder();
 		
-		Method[] methods = skillRegistration.getClass().getMethods();
+		Method[] methods = skill.getClass().getMethods();
 		for (int i = 0; i < methods.length; i++) {
 
 			for (States state : States.values()) {
 				if (methods[i].isAnnotationPresent(state.getKey())) {
 
-					SkillAction action = new SkillAction(methods[i], skillRegistration);
+					SkillAction action = new SkillAction(methods[i], skill);
 					stateMachineBuilder.withAction(action, state.getStateName()); 
 				}
 			}
