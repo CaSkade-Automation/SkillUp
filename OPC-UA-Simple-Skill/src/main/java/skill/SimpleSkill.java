12b18fa787fa2e0ac6bb2ac59037b46e75eaf2c1
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-04-09 10:11:28 +0200

    Because of tracking the bundles, the skill is no longer a Component. The parameter j is now annotated with SkillInput.

--- OPC-UA-Simple-Skill/src/main/java/skill/SimpleSkill.java
+++ OPC-UA-Simple-Skill/src/main/java/skill/SimpleSkill.java
@@ -1,32 +1,25 @@
 package skill;
 
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import annotations.Completing;
 import annotations.Execute;
+import annotations.SkillInput;
 import annotations.Skill;
 import annotations.Starting;
 
-/**
- * @Component Indicates that annotated class is intended to be an OSGi
- *            component. <br>
- *            After becoming satisfied component is registered as a service
- *            under OPCUASkillRegistration
- */
 @Skill
-@Component(factory = "skill.factory")
 public class SimpleSkill {
 
 	private final Logger logger = LoggerFactory.getLogger(SimpleSkill.class);
-	private int j = 0;
-
+	
+	@SkillInput
+	private int j;
+	
 	@Starting
 	public void starting() {
-		System.out.println("Starting, j = " + j);
+		logger.info("Starting, j = " + j);
 	}
 
 	@Execute
@@ -36,17 +29,6 @@ public class SimpleSkill {
 
 	@Completing
 	public void completing() {
-		System.out.println("Completing, j = " + j);
-	}
-
-
-	@Activate
-	public void activate() {
-		logger.info("OPC-UA-Skill zum Addieren wird aktiviert.");
-	}
-
-	@Deactivate
-	public void deactivate() {
-		logger.info("OPC-UA-Skill zum Addieren wird deaktiviert.");
+		logger.info("Completing, j = " + j);
 	}
 }
