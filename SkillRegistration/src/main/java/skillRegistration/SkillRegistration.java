014ad1d52566be99681fbd959fad437d371600c6
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-06-08 21:18:09 +0200

    SkillRegistration extends from Registration and no longer has the bundle tracking functions. Otherwise it has the methods from SmartModule to register and delete a skill.

--- SkillRegistration/src/main/java/skillRegistration/SkillRegistration.java
+++ SkillRegistration/src/main/java/skillRegistration/SkillRegistration.java
@@ -1,194 +1,87 @@
 package skillRegistration;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleEvent;
-import org.osgi.framework.wiring.BundleWiring;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.util.tracker.BundleTracker;
-import org.osgi.util.tracker.BundleTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import actionGenerator.ActionGenerator;
 import annotations.Skill;
-import server.Server;
-import skillDescriptionGeneratorInterface.SkillDescriptionGenerator;
-import skillGeneratorInterface.SkillGeneratorInterface;
-import smartModule.SmartModule;
-import statemachine.StateMachine;
+import opsDescription.OpsDescription;
+import registration.Registration;
 
-@Component(immediate = true)
-public class SkillRegistration {
+@Component(immediate = true, service = SkillRegistration.class)
+public class SkillRegistration extends Registration {
 
-	private final Logger logger = LoggerFactory.getLogger(SkillRegistration.class);
+	private Logger logger = LoggerFactory.getLogger(SkillRegistration.class);
 
-	private BundleTracker<Bundle> bundleTracker;
-	private Map<Bundle, Object> classObjects = new HashMap<Bundle, Object>();
+	@Override
+	public void register(String requestBody, Object object) {
+		// TODO Auto-generated method stub
+		String moduleIri = getModules().get(skillNeedsModule(object.getClass().getAnnotation(Skill.class).namespace()));
 
-	@Reference
-	ActionGenerator actionGenerator;
+		logger.info("Registering Skill " + object.getClass().getSimpleName() + " with module " + moduleIri);
 
-	@Reference(target = "(name=OpcUa)", cardinality = ReferenceCardinality.OPTIONAL)
-	volatile SkillGeneratorInterface opcUaSkillGenerator;
+		for (OpsDescription myOpsDescription : getOpsDescriptionList()) {
 
-	@Reference(target = "(name=Rest)", cardinality = ReferenceCardinality.OPTIONAL)
-	volatile SkillGeneratorInterface restSkillGenerator;
+			String moduleEndpoint = myOpsDescription.getModuleEndpoint();
+			String moduleIriEncoded = encodeValue(moduleIri);
+			String capabilityEndpoint = myOpsDescription.getCapabilityEndpoint();
+			String location = moduleEndpoint + "/" + moduleIriEncoded + capabilityEndpoint;
 
-	@Reference
-	SkillDescriptionGenerator skillDescriptionGenerator;
+			int responseStatusCode = opsRequest(myOpsDescription, "POST", location, requestBody);
 
-	@Reference
-	SmartModule module;
-	
-	@Reference
-	Server server; 
+			if (responseStatusCode == 201) {
+				logger.info("Skill " + object.getClass().getSimpleName() + " registered to " + "OPS "
+						+ myOpsDescription.getId());
+				ArrayList<String> skills = getOpsAndSkillList().get(myOpsDescription.getId());
+				skills.add(object.getClass().getSimpleName());
+				getOpsAndSkillList().put(myOpsDescription.getId(), skills);
+				opsSkillListMessage();
 
-	@Activate
-	public void activate(BundleContext context) {
-
-		this.bundleTracker = new BundleTracker<Bundle>(context, Bundle.ACTIVE, new BundleTrackerCustomizer<Bundle>() {
-
-			@Override
-			public Bundle addingBundle(Bundle bundle, BundleEvent event) {
-				// TODO Auto-generated method stub
-
-				if (bundle.getLocation().contains("include")) {
-
-					BundleWiring wiring = bundle.adapt(BundleWiring.class);
-					if (wiring != null) {
-
-						Collection<String> resources = wiring.listResources("/", "*class",
-								BundleWiring.LISTRESOURCES_RECURSE);
-
-						for (String resource : resources) {
-
-							try {
-								resource = resource.replace("/", ".");
-								resource = resource.substring(0, resource.lastIndexOf("."));
-								Class<?> skill = bundle.loadClass(resource);
-								// Class.forName(resource);
-								Skill skillAnnotation = skill.getAnnotation(Skill.class);
-
-								if (skillAnnotation != null) {
-
-									Object skillObj = skill.getDeclaredConstructor().newInstance();
-									classObjects.put(bundle, skillObj);
-									StateMachine stateMachine = actionGenerator.generateAction(skillObj);
-
-									if (skillAnnotation.value().equals("OpcUaSkill")) {
-										logger.info("Add OPC-UA-Skill");
-
-										opcUaSkillGenerator.generateSkill(skillObj, stateMachine);
-										Enumeration<String> userSnippets = bundle.getEntryPaths("ExampleSnippet");
-										String skillDescription = skillDescriptionGenerator
-												.generateOpcUaDescription(server, skillObj, stateMachine, userSnippets);
-										module.registerSkill(skillDescription, skillObj.getClass().getSimpleName());
-
-									} else if (skillAnnotation.value().equals("RestSkill")) {
-										logger.info("Add REST-Skill");
-
-										restSkillGenerator.generateSkill(skillObj, stateMachine);
-									}
-								}
-							} catch (ClassNotFoundException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (SecurityException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (InstantiationException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IllegalAccessException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IllegalArgumentException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (InvocationTargetException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (NoSuchMethodException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-				return bundle;
+			} else {
+				logger.info("Skill: " + object.getClass().getSimpleName() + " couldn't be registered to " + "OPS "
+						+ myOpsDescription.getId());
 			}
-
-			@Override
-			public void modifiedBundle(Bundle bundle, BundleEvent event, Bundle object) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void removedBundle(Bundle bundle, BundleEvent event, Bundle object) {
-
-				BundleWiring wiring = bundle.adapt(BundleWiring.class);
-				if (wiring != null) {
-
-					Collection<String> resources = wiring.listResources("/", "*class",
-							BundleWiring.LISTRESOURCES_RECURSE);
-
-					for (String resource : resources) {
-						try {
-							resource = resource.replace("/", ".");
-							resource = resource.substring(0, resource.lastIndexOf("."));
-							Class<?> skill = bundle.loadClass(resource);
-							// Class.forName(resource);
-							Skill skillAnnotation = skill.getAnnotation(Skill.class);
-
-							if (skillAnnotation != null) {
-
-								Object skillObj = classObjects.get(bundle);
-								// hier fehlt noch delete action!
-
-								if (skillAnnotation.value().equals("OpcUaSkill")) {
-									logger.info("Delete OPC-UA-Skill");
-
-									opcUaSkillGenerator.deleteSkill(skillObj);
-
-								} else if (skillAnnotation.value().equals("RestSkill")) {
-									logger.info("Delete REST-Skill");
-
-									restSkillGenerator.deleteSkill(skillObj);
-								}
-								classObjects.remove(bundle);
-							}
-						} catch (ClassNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (SecurityException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IllegalArgumentException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		});
-		this.bundleTracker.open();
-
+		}
 	}
 
-	@Deactivate
-	public void deactivate() {
-		this.bundleTracker.close();
+//	public void stateChanged(Object skill, IState state) {
+//	String location = moduleEndpoint + "/" + moduleIri + capabilityEndpoint + "/"
+//			+ stateEndpoint;
+//
+//	for (OpsDescription myOpsDescription : opsDescriptionList) {
+//		int responseStatusCode = sendSPARQLQuery(myOpsDescription, "POST", location, state);
+//	}
+//}
+
+	@Override
+	public void delete(Object object) {
+		// TODO Auto-generated method stub
+		String name = object.getClass().getSimpleName();
+		logger.info("Unregistering Skill " + name + "...");
+
+		for (OpsDescription myOpsDescription : getOpsDescriptionList()) {
+
+			// String moduleEndpoint = myOpsDescription.getModuleEndpoint();
+			String capabilityIri = getModules()
+					.get(skillNeedsModule(object.getClass().getAnnotation(Skill.class).namespace())) + "_" + name + "_"
+					+ "Process";
+			capabilityIri = encodeValue(capabilityIri);
+			String capabilityEndpoint = myOpsDescription.getCapabilityEndpoint();
+			String location = capabilityEndpoint + "/" + capabilityIri;
+			// String location = moduleEndpoint + "/" + capabilityIri + capabilityEndpoint +
+			// "/" + serviceName;
+
+			// When OPS is capable of deleting a skill which response status code do you
+			// get????
+			opsRequest(myOpsDescription, "DELETE", location, "");
+
+			logger.info("Skill " + name + " removed from " + myOpsDescription.getId());
+			ArrayList<String> skills = getOpsAndSkillList().get(myOpsDescription.getId());
+			skills.remove(name);
+			getOpsAndSkillList().put(myOpsDescription.getId(), skills);
+			opsSkillListMessage();
+		}
 	}
 }
