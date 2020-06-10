d59519c28686b71191ca306a0fbdb0ae29d435e7
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-06-10 12:13:43 +0200

    Snippets are improved with module IRI (neither mac address nor names needed). Now user turtle snippets are used too.

--- Module-Description-Generator/src/main/java/moduleDescriptionGenerator/ModuleDescriptionGenerator.java
+++ Module-Description-Generator/src/main/java/moduleDescriptionGenerator/ModuleDescriptionGenerator.java
@@ -1,6 +1,7 @@
 package moduleDescriptionGenerator;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 
 import org.eclipse.milo.opcua.sdk.server.OpcUaServer;
@@ -14,41 +15,41 @@ import server.Server;
 @Component(immediate = true, service = ModuleDescriptionGenerator.class)
 public class ModuleDescriptionGenerator extends DescriptionGenerator {
 
-	private String registrationPrefixSnippet = "@prefix registration: <${Namespace}/modules#> .";
+	private String moduleSnippet = "${ModuleIri} a VDI3682:Module,\r\n" + "					owl:NamedIndividual. ";
 
-	private String moduleSnippet = "registration:${MACAddress}_${ModuleName} a VDI3682:Module,\r\n"
-			+ "					owl:NamedIndividual. ";
+	private String capabilitySnippet = "${ModuleIri} Cap:hasCapability ${CapabilityIri} .";
 
-	private String opcUaServerSnippet = "module:_${ServerName} a OpcUa:UAServer,\r\n"
+	private String opcUaServerSnippet = "${ModuleIri}_${ServerName} a OpcUa:UAServer,\r\n"
 			+ "						owl:NamedIndividual;\r\n"
 			+ "					OpcUa:hasNodeSet module:_${ServerName}_NodeSet.\r\n"
-			+ "module:_${ServerName}_NodeSet a OpcUa:UANodeSet, \r\n"
+			+ "${ModuleIri}_${ServerName}_NodeSet a OpcUa:UANodeSet, \r\n"
 			+ "									owl:NamedIndividual. \r\n" + "";
 
-	private String opcUaServerSecuritySnippet = "module:_${ServerName} OpcUa:hasEndpointUrl \"${EndpointUrl}\";\r\n"
+	private String opcUaServerSecuritySnippet = "${ModuleIri}_${ServerName} OpcUa:hasEndpointUrl \"${EndpointUrl}\";\r\n"
 			+ "					OpcUa:requiresUserName \"${UserName}\";\r\n"
 			+ "					OpcUa:requiresPassword \"${Password}\";\r\n"
 			+ "					OpcUa:hasMessageSecurityMode OpcUa:MessageSecurityMode_${MessageSecurityMode}; \r\n"
 			+ "					OpcUa:hasSecurityPolicy OpcUa:${SecurityPolicy} .";
 
-	public String generateModuleDescription(Object module, Server server) {
+	public String generateModuleDescription(Object module, Server server, Enumeration<String> userFiles) {
 
 		try {
 			String prefix = getFileFromResources(null, "prefix.ttl");
 
-			String macAddress = getMacAddress();
-			setMacAddress(macAddress);
-
-			String capabilityDescription = generateCapabilityDescription(module, true);
-
 			String opcUaServerDescription = generateOpcUaServerDescription(server);
 
 			Module moduleAnnotation = module.getClass().getAnnotation(Module.class);
+			
+			String userSnippet = getUserSnippets(userFiles, module.getClass().getClassLoader());
 
-			String moduleDescription = prefix + registrationPrefixSnippet + getModulePrefixSnippet() + moduleSnippet
-					+ capabilityDescription + opcUaServerDescription;
-			moduleDescription = moduleDescription.replace("${Namespace}", moduleAnnotation.namespace())
-					.replace("${ModuleName}", moduleAnnotation.name()).replace("${MACAddress}", macAddress);
+			String moduleDescription = prefix + moduleSnippet + opcUaServerDescription + userSnippet;
+
+			if (!moduleAnnotation.capabilityIri().isEmpty()) {
+				moduleDescription = moduleDescription + capabilitySnippet;
+			}
+
+			moduleDescription = moduleDescription.replace("${ModuleIri}", moduleAnnotation.moduleIri())
+					.replace("${CapabilityIri}", moduleAnnotation.capabilityIri());
 			try {
 				createFile(moduleDescription, "module.ttl");
 			} catch (IOException e) {
