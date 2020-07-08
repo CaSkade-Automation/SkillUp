550589447f792ded16e9d9110796e25a8cad95b1
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-07-08 17:19:58 +0200

    SkillGeneratorInterface added method to generate description, because Skill Generators now contain skill and description generation in one bundle.

--- Skill-Generator-Interface/src/main/java/skillGeneratorInterface/SkillGeneratorInterface.java
+++ Skill-Generator-Interface/src/main/java/skillGeneratorInterface/SkillGeneratorInterface.java
@@ -1,9 +1,12 @@
 package skillGeneratorInterface;
 
+import java.util.Enumeration;
+
 import statemachine.StateMachine;
 
 public interface SkillGeneratorInterface {
 
 	public void generateSkill(Object skill, StateMachine stateMachine); 
+	public String generateDescription(Object skill, StateMachine stateMachine, Enumeration<String> userFiles); 
 	public void deleteSkill(Object skill); 
 }
