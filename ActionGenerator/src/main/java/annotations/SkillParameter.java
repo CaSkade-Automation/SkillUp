af712479ecd3ee4945aa062e3af8ae8e0c11f544
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-07-08 16:32:59 +0200

    Changed name of SkillInput to SkillParameter

--- ActionGenerator/src/main/java/annotations/SkillParameter.java
+++ ActionGenerator/src/main/java/annotations/SkillParameter.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface SkillInput {
+public @interface SkillParameter {
 
 	public String name() default "";   
 	public String description() default "";  
