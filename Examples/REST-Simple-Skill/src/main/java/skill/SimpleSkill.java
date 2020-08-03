8c54928682975a0b6a8e113c7e979b1baf007176
Marco Kaltwasser <marco.kaltwasser@hsu-hh.de>
2020-08-03 13:04:02 +0200

    FIX: Example Rest Skill has type RestSkill

--- Examples/REST-Simple-Skill/src/main/java/skill/SimpleSkill.java
+++ Examples/REST-Simple-Skill/src/main/java/skill/SimpleSkill.java
@@ -10,7 +10,7 @@ import annotations.SkillOutput;
 import annotations.Skill;
 import annotations.Starting;
 
-@Skill(skillIri = "https://siemens.de/skills#RestSkill", capabilityIri = "https://siemens.de/capabilites#bestCapability", moduleIri = "https://siemens.de/modules#ModuleA")
+@Skill(type = "RestSkill", skillIri = "https://siemens.de/skills#RestSkill", capabilityIri = "https://siemens.de/capabilites#bestCapability", moduleIri = "https://siemens.de/modules#ModuleA")
 public class SimpleSkill {
 
 	private final Logger logger = LoggerFactory.getLogger(SimpleSkill.class);
