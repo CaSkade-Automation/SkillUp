bbb66fef006776fe2858b4d7259158c91e14dcb0
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 09:32:33 +0100

    Annotations for module and skill without field name but with field description. Type of skill is an class which extends from class SkillType (easier to add new skill generators for other technologies).

--- Action-Generator/src/main/java/annotations/Module.java
+++ Action-Generator/src/main/java/annotations/Module.java
@@ -5,12 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation Module needs a moduleIRI. Description and capabilityIRI are
+ * optional.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Module {
 
-	public String name(); 
-	public String description() default ""; 
-	public String moduleIri(); 
-	public String capabilityIri() default ""; 
+	public String description() default "";
+
+	public String moduleIri();
+
+	public String capabilityIri() default "";
 }
