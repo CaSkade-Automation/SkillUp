ac08cd7a7c3ab2f20af6cd21f0466b003dce51dd
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 11:09:32 +0100

    Added comments and deleted unnecessary methods.

--- Registration/src/main/java/registration/ModuleRegistry.java
+++ Registration/src/main/java/registration/ModuleRegistry.java
@@ -8,23 +8,39 @@ import org.slf4j.LoggerFactory;
 
 import annotations.Module;
 
+/**
+ * Class manages OPS list
+ */
 public class ModuleRegistry {
 
 	private List<OpsDescription> opsDescriptionList = new ArrayList<OpsDescription>();
 	private Logger logger = LoggerFactory.getLogger(ModuleRegistry.class);
 
+	/**
+	 * Method to get list of all OPS to which module is already registered
+	 * 
+	 * @param moduleIri module to which skill is connected
+	 * @return list of OPS to which skill has to be registered
+	 */
 	public List<OpsDescription> skillRegisterOpsList(String moduleIri) {
 		List<OpsDescription> opsDescriptionSkillList = new ArrayList<OpsDescription>();
 		for (OpsDescription opsDescription : opsDescriptionList) {
 			for (Object module : opsDescription.getModules()) {
 				if (module.getClass().getAnnotation(Module.class).moduleIri().equals(moduleIri)) {
 					opsDescriptionSkillList.add(opsDescription);
+					break;
 				}
 			}
 		}
 		return opsDescriptionSkillList;
 	}
 
+	/**
+	 * Checks if module to which skill is associated is already registered
+	 * 
+	 * @param moduleIri module which should be already registered
+	 * @return boolean value, true if module already registered otherwise false
+	 */
 	public boolean skillNeedsModule(String moduleIri) {
 		for (OpsDescription opsDescription : opsDescriptionList) {
 			for (Object module : opsDescription.getModules()) {
@@ -58,8 +74,7 @@ public class ModuleRegistry {
 	}
 
 	/**
-	 * Method to add new OPS (when module is successfully registered to it) to
-	 * OPS-list
+	 * Method to add new OPS to OPS-list (when broadcast successful)
 	 * 
 	 * @param opsDescription description of OPS
 	 */
@@ -69,7 +84,7 @@ public class ModuleRegistry {
 	}
 
 	/**
-	 * Method to remove OPS (when OPS deleted module) from OPS-list
+	 * Method to remove OPS from OPS-list (when OPS no longer available)
 	 * 
 	 * @param remoteAddr IP address of OPS
 	 * @return If OPS could be removed: true <br>
@@ -91,13 +106,5 @@ public class ModuleRegistry {
 		for (OpsDescription ops : opsDescriptionList) {
 			logger.info(ops.getId());
 		}
-	}
-
-	public void addModule(OpsDescription opsDescription, Object module) {
-		opsDescription.addModule(module);
-	}
-
-	public void deleteModule(OpsDescription opsDescription, Object module) {
-		opsDescription.deleteModule(module);
 	}
 }
