f5049f19d8ff6620b5bcf6201ac02259410bd4dc
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 09:36:45 +0100

    New class SkillType. For now two classes which extend SkillType (REST and OpcUa). New skills or skill generators have to take one of the classes (Skill: in annotation @Skill; SkillGenerator: in property of Component) which extend class SkillType.

---
+++ Action-Generator/src/main/java/actionGenerator/OpcUaSkillType.java
@@ -0,0 +1,9 @@
+package actionGenerator;
+
+/**
+ * Class to tell that new skill is of type OPC-UA when this class is set as type
+ * of skill in @Skill Annotation
+ */
+public class OpcUaSkillType extends SkillType {
+
+}
