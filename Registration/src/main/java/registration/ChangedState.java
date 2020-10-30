acbcccb3ee5adc5eb509031689dc451d80e1012c
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 10:45:00 +0100

    Added comments.

--- Registration/src/main/java/registration/ChangedState.java
+++ Registration/src/main/java/registration/ChangedState.java
@@ -1,5 +1,8 @@
 package registration;
 
+/**
+ * Class of new state to convert easier to json with gson
+ */
 public class ChangedState {
-	public String newState = null; 
+	public String newState = null;
 }
