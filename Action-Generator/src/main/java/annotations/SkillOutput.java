8da08a2dbafb99167c69a58a6b700fe0c42ffcd2
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 09:27:35 +0100

    Added comments and added exceptions to existing catch clause instead of single catch clauses. Changed class variables to private.

--- Action-Generator/src/main/java/annotations/SkillOutput.java
+++ Action-Generator/src/main/java/annotations/SkillOutput.java
@@ -5,11 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * SkillOutput annotation can have a name and description. Necessary is a
+ * boolean value for isRequired which tells if this parameter is essential for
+ * executing a skill method
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface SkillOutput {
 
-	public String name() default ""; 
-	public String description() default ""; 
-	public boolean isRequired(); 
+	public String name() default "";
+
+	public String description() default "";
+
+	public boolean isRequired();
 }
