579849cd1583b461c1c89c8f538d3ae29e512b1c
Miguel Vieira <miguel.vieira@hsu-hh.de>
2020-10-30 09:45:46 +0100

    Added comments and simplified some things (try and catch clause no longer necessary).

--- Module-Generator/src/main/java/moduleGenerator/ModuleGenerator.java
+++ Module-Generator/src/main/java/moduleGenerator/ModuleGenerator.java
@@ -1,6 +1,5 @@
 package moduleGenerator;
 
-import java.io.IOException;
 import java.util.Enumeration;
 
 import org.osgi.service.component.annotations.Component;
@@ -8,41 +7,50 @@ import org.osgi.service.component.annotations.Component;
 import annotations.Module;
 import descriptionGenerator.DescriptionGenerator;
 
+/**
+ * Class to generate module description
+ * 
+ * @Component Indicates that annotated class is intended to be an OSGi
+ *            component. <br>
+ *            immediate=true, component configuration activates immediately.
+ *            <br>
+ *            After becoming satisfied component is registered as a service.
+ */
 @Component(immediate = true, service = ModuleGenerator.class)
 public class ModuleGenerator extends DescriptionGenerator {
 
+	// neessary/possible snippets for a module description
 	private String moduleSnippet = "<${ModuleIri}> a VDI2206:Module,\r\n" + "					owl:NamedIndividual. ";
-
 	private String capabilitySnippet = "<${ModuleIri}> Cap:hasCapability <${CapabilityIri}> .";
 
+	/**
+	 * Method to generate rdf description. Takes moduleSnippet and replaces dummies
+	 * like module IRI.
+	 * 
+	 * @param module    object necessary to e.g. get module IRI
+	 * @param userFiles if user has it own snippets, they are added to the
+	 *                  description
+	 * @return
+	 */
 	public String generateModuleDescription(Object module, Enumeration<String> userFiles) {
 
-		try {
-			String prefix = getFileFromResources(null, "prefix.ttl");
+		// first get all prefixes
+		String prefix = getFileFromResources(null, "prefix.ttl");
 
-			Module moduleAnnotation = module.getClass().getAnnotation(Module.class);
+		Module moduleAnnotation = module.getClass().getAnnotation(Module.class);
 
-			String userSnippet = getUserSnippets(userFiles, module.getClass().getClassLoader());
+		String userSnippet = getUserSnippets(userFiles, module.getClass().getClassLoader());
 
-			String moduleDescription = prefix + moduleSnippet + userSnippet;
+		// whole module description
+		String moduleDescription = prefix + moduleSnippet + userSnippet;
 
-			if (!moduleAnnotation.capabilityIri().isEmpty()) {
-				moduleDescription = moduleDescription + capabilitySnippet;
-			}
-
-			moduleDescription = moduleDescription.replace("${ModuleIri}", moduleAnnotation.moduleIri())
-					.replace("${CapabilityIri}", moduleAnnotation.capabilityIri());
-			try {
-				createFile(moduleDescription, "module.ttl");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return moduleDescription;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
+		if (!moduleAnnotation.capabilityIri().isEmpty()) {
+			moduleDescription = moduleDescription + capabilitySnippet;
 		}
+
+		// replace dummies
+		moduleDescription = moduleDescription.replace("${ModuleIri}", moduleAnnotation.moduleIri())
+				.replace("${CapabilityIri}", moduleAnnotation.capabilityIri());
+		return moduleDescription;
 	}
 }
