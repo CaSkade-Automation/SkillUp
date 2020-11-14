c9a2985aecf0ec60e863671711d021be020c56ee
Marco Kaltwasser <mkaltw@protonmail.ch>
2020-11-14 18:36:21 +0100

    ADD: Method to POST new SkillParams as JSON object

--- REST-Resource/src/main/java/restResource/RestResource.java
+++ REST-Resource/src/main/java/restResource/RestResource.java
@@ -1,13 +1,16 @@
 package restResource;
 
+import java.io.StringReader;
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.UUID;
 
 import javax.json.Json;
+import javax.json.JsonNumber;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -114,7 +117,7 @@ public class RestResource {
 		}
 
 		String responseString = objBuilder.build().toString();
-		// This may return an empty json object (with status code OK)!
+		// This may return an empty JSON object (with status code OK)!
 		return Response.status(Response.Status.OK).entity(responseString).build();
 	}
 
@@ -160,16 +163,24 @@ public class RestResource {
 					if (field.isAnnotationPresent(SkillOutput.class)) {
 						field.setAccessible(true);
 						JsonObjectBuilder jsonField = Json.createObjectBuilder();
-						jsonField.add("name", field.getAnnotation(SkillOutput.class).name()).add("type", field.getType().getSimpleName()).add("isRequired", Boolean.toString(field.getAnnotation(SkillOutput.class).isRequired())).add("description", field.getAnnotation(SkillOutput.class).description());
+						jsonField.add("name", field.getAnnotation(SkillOutput.class).name())
+								.add("type", field.getType().getSimpleName())
+								.add("isRequired",
+										Boolean.toString(field.getAnnotation(SkillOutput.class).isRequired()))
+								.add("description", field.getAnnotation(SkillOutput.class).description());
 						try {
 							String fieldValue = field.get(rSkill.getSkillObject()).toString();
 							jsonField.add("value", fieldValue);
 						} catch (IllegalArgumentException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillOutput (value of output, IllegalArgumentException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillOutput (value of output, IllegalArgumentException)"
+									+ e);
 						} catch (IllegalAccessException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillOutput (value of output, IllegalAccessException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillOutput (value of output, IllegalAccessException)" + e);
 						} catch (NullPointerException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillOutput (value of output, NullPointerException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillOutput (value of output, NullPointerException)" + e);
 							jsonField.add("value", "null");
 						}
 						objBuilder.add(field.getName(), jsonField);
@@ -204,16 +215,25 @@ public class RestResource {
 					if (field.isAnnotationPresent(SkillParameter.class)) {
 						field.setAccessible(true);
 						JsonObjectBuilder jsonField = Json.createObjectBuilder();
-						jsonField.add("name", field.getAnnotation(SkillParameter.class).name()).add("type", field.getType().getSimpleName()).add("isRequired", Boolean.toString(field.getAnnotation(SkillParameter.class).isRequired())).add("description", field.getAnnotation(SkillParameter.class).description());
+						jsonField.add("name", field.getAnnotation(SkillParameter.class).name())
+								.add("type", field.getType().getSimpleName())
+								.add("isRequired",
+										Boolean.toString(field.getAnnotation(SkillParameter.class).isRequired()))
+								.add("description", field.getAnnotation(SkillParameter.class).description());
 						try {
 							String fieldValue = field.get(rSkill.getSkillObject()).toString();
 							jsonField.add("value", fieldValue);
 						} catch (IllegalArgumentException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillParameter (value of param, IllegalArgumentException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillParameter (value of param, IllegalArgumentException)"
+									+ e);
 						} catch (IllegalAccessException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillParameter (value of param, IllegalAccessException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillParameter (value of param, IllegalAccessException)"
+									+ e);
 						} catch (NullPointerException e) {
-							logger.error(getClass().getSimpleName() + ": ERR on generating skillParameter (value of param, NullPointerException)" + e);
+							logger.error(getClass().getSimpleName()
+									+ ": ERR on generating skillParameter (value of param, NullPointerException)" + e);
 							jsonField.add("value", "null");
 						}
 						objBuilder.add(field.getName(), jsonField);
@@ -228,6 +248,143 @@ public class RestResource {
 		// no match found!
 		return Response.status(Response.Status.NOT_FOUND).build();
 	}
+
+	// Caution: Matches the SkillParams by Cap:hasVariableName
+	// Example Input = { "i": "HelloParam", "j": 42 }
+	// Able to process:
+	// Strings
+	// Doubles
+	// Ints
+	// Booleans
+	// TODO: Better way?
+	@POST
+	@Path("{uid}/skillParameters")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response setSkillParameters(@PathParam("uid") String uid, String body) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+
+		UUID skillKey = UUID.fromString(uid);
+
+		JsonReader jsonReader = Json.createReader(new StringReader(body));
+		JsonObject jobj = jsonReader.readObject();
+
+		// idea: We iterate over all fields of the skill that matches the UUID
+		// check if the field is annotated with @SkillParameter
+		// for every annotated field we iterate through JSON object
+		// and check if the jsonKey matches with the variableName
+		// match => update the variable's value
+
+		// may need to do some casting magic...
+
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(skillKey)) {
+				// we found the correct skill by UUID
+				Field[] fields = rSkill.getSkillObject().getClass().getDeclaredFields();
+				for (Field field : fields) {
+					if (field.isAnnotationPresent(SkillParameter.class)) {
+						field.setAccessible(true);
+						// we found a SkillParameter
+						// now go through all sent Params in JSON object
+						// and see if there is match (variableName and JSON key)
+						for (String jsonKey : jobj.keySet()) {
+							if (jsonKey.equals(field.getName())) {
+								// variableName matches JSON key
+								// -> update that variable's value!
+								logger.info(getClass().getSimpleName() + ": UPDATE SKILL PARAM (" + field.getName()
+										+ " to " + jobj.get(jsonKey).toString() + ")");
+								boolean success = updateSkillParam(rSkill.getSkillObject(), field, jobj.get(jsonKey));
+								objBuilder.add(jsonKey, success);
+								// break because we updated that field!
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		String responseString = objBuilder.build().toString();
+		return Response.status(Response.Status.OK).entity(responseString).build();
+	}
+
+	// Idea: we cast any JsonValue to a String and then convert that String to the
+	// Field's Type!
+	private boolean updateSkillParam(Object skillObject, Field toSet, JsonValue jsonValue) {
+		if (toSet.getType().equals(Integer.class) || toSet.getType().equals(int.class)) {
+			logger.info("INTEGER");
+			if (jsonValue instanceof JsonNumber) {
+				try {
+					toSet.setInt(skillObject, Integer.valueOf(((JsonNumber) jsonValue).toString()));
+					return true;
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					logger.error("Error while casting JsonValue to Integer");
+					logger.error(e.toString());
+					return false;
+				}
+			} else {
+				logger.error("JsonValue not instanceof JsonNumber!");
+				return false;
+			}
+		}
+		if (toSet.getType().equals(Double.class) || toSet.getType().equals(double.class)) {
+			logger.info("DOUBLE");
+			if (jsonValue instanceof JsonNumber) {
+				try {
+					toSet.setDouble(skillObject, Double.valueOf(((JsonNumber) jsonValue).toString()));
+					return true;
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					logger.error("Error while casting JsonValue to Double");
+					logger.error(e.toString());
+					return false;
+				}
+			} else {
+				logger.error("JsonValue not instanceof JsonNumber!");
+				return false;
+			}
+		}
+		if (toSet.getType().equals(Boolean.class) || toSet.getType().equals(boolean.class)) {
+			logger.info("BOOLEAN");
+			if (jsonValue == JsonValue.TRUE || jsonValue == JsonValue.FALSE) {
+				try {
+					toSet.setBoolean(skillObject, Boolean.getBoolean(jsonValue.toString()));
+					return true;
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					logger.error("Error while casting JsonValue to Boolean");
+					logger.error(e.toString());
+					return false;
+				}
+			} else {
+				logger.error("JsonValue not a Boolean!");
+				return false;
+			}
+		}
+		if (toSet.getType().equals(String.class)) {
+			logger.info("STRING");
+			if (jsonValue.getValueType() == JsonValue.ValueType.STRING) {
+				try {
+					String value = jsonValue.toString();
+					// we strip first and last index to remove quotes!
+					value = value.substring(1, value.length() - 1);
+					toSet.set(skillObject, value);
+					return true;
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					logger.error("Error while casting JsonValue to String");
+					logger.error(e.toString());
+					return false;
+				}
+			} else {
+				logger.error("JsonValue not a String!");
+				return false;
+			}
+		}
+		logger.error("End of updateSkillParam -> unknown Datatype?");
+		return false;
+	}
+
 	@POST
 	@Path("{uid}/start")
 	@Produces(MediaType.APPLICATION_JSON)
