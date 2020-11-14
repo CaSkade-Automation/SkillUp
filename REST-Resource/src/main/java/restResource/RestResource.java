09e58135280ff2861e3901c0e717e03d351d6364
Marco Kaltwasser <mkaltw@protonmail.ch>
2020-11-14 12:46:05 +0100

    REFACTOR: RestSkill stateMachine interaction via POST requests. They now return skill info just like info method does (including OK, Bad Request, Not Found)

--- REST-Resource/src/main/java/restResource/RestResource.java
+++ REST-Resource/src/main/java/restResource/RestResource.java
@@ -1,7 +1,6 @@
 package restResource;
 
 import java.util.HashMap;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.json.Json;
@@ -108,8 +107,9 @@ public class RestResource {
 			objBuilder.add(rSkill.getSkillIri(), Json.createObjectBuilder().add("state", rSkill.getState()).add("uuid",
 					rSkill.getUUID().toString()));
 		}
-		
+
 		String responseString = objBuilder.build().toString();
+		// This may return an empty json object (with status code OK)!
 		return Response.status(Response.Status.OK).entity(responseString).build();
 	}
 
@@ -117,365 +117,238 @@ public class RestResource {
 	@Path("{uid}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response info(@PathParam("uid") String uid) {
-
 		if (!isValidUUID(uid)) {
 			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
 		UUID key = UUID.fromString(uid);
-		
+
 		for (RestSkill rSkill : skillDirectory.values()) {
 			if (rSkill.getUUID().equals(key)) {
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder().add("state", rSkill.getState()).add("uuid",
-						rSkill.getUUID().toString())).build();
-				
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
+
 				String responseString = obj.toString();
 				return Response.status(Response.Status.OK).entity(responseString).build();
 			}
 		}
-		
+
 		// no match found!
 		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/start")
-	@Produces(MediaType.TEXT_HTML)
-	public String start(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response start(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " START page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the START page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.start();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually start the skill
-			skillTable.get(key).start();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/reset")
-	@Produces(MediaType.TEXT_HTML)
-	public String reset(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response reset(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " RESET page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the RESET page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.reset();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually reset the skill
-			skillTable.get(key).reset();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/hold")
-	@Produces(MediaType.TEXT_HTML)
-	public String hold(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response hold(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " HOLD page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the HOLD page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.hold();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually hold the skill
-			skillTable.get(key).hold();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/unhold")
-	@Produces(MediaType.TEXT_HTML)
-	public String unhold(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response unhold(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " UNHOLD page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the UNHOLD page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.unhold();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually unhold the skill
-			skillTable.get(key).unhold();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/suspend")
-	@Produces(MediaType.TEXT_HTML)
-	public String suspend(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response suspend(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " SUSPEND page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the SUSPEND page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.suspend();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually suspend the skill
-			skillTable.get(key).suspend();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/unsuspend")
-	@Produces(MediaType.TEXT_HTML)
-	public String unsuspend(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response unsuspend(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " UNSUSPEND page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the UNSUSPEND page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.unsuspend();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually unsuspend the skill
-			skillTable.get(key).unsuspend();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/stop")
-	@Produces(MediaType.TEXT_HTML)
-	public String stop(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response stop(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " STOP page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the STOP page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.stop();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually stop the skill
-			skillTable.get(key).stop();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/abort")
-	@Produces(MediaType.TEXT_HTML)
-	public String abort(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response abort(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " ABORT page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the ABORT page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.abort();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually abort the skill
-			skillTable.get(key).abort();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	@POST
 	@Path("{uid}/clear")
-	@Produces(MediaType.TEXT_HTML)
-	public String clear(@PathParam("uid") String uid) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response clear(@PathParam("uid") String uid) {
+		if (!isValidUUID(uid)) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		UUID key = UUID.fromString(uid);
-		logger.info(getClass().getSimpleName() + ": RestSkill # " + key + " CLEAR page called!");
 
-		StringBuilder sb = new StringBuilder("<html><body>");
-		sb.append("<h1>Simple RESTful OSGi StateMachine</h1>");
-		sb.append("<h2>You called the CLEAR page for RestSkill # " + key + "</h2>");
+		for (RestSkill rSkill : skillDirectory.values()) {
+			if (rSkill.getUUID().equals(key)) {
+				rSkill.clear();
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
 
-		if (skillTable.containsKey(key)) {
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\", UUID: \"" + skillTable.get(key).getUUID() + "\", skillIri: \""
-					+ skillTable.get(key).getSkillIri() + "\", State: \"" + skillTable.get(key).getState() + "\"");
-			sb.append("</p>");
-
-			sb.append("<p>");
-			sb.append("You can check the skill again to see if you were successful.");
-			sb.append("</p>");
-
-			// actually clear the skill
-			skillTable.get(key).clear();
-
-			sb.append("<p>");
-			sb.append("http://localhost:8181/skills/" + key);
-			sb.append("</p>");
-		} else {
-			// key does not exist
-			sb.append("<p>");
-			sb.append("Key: \"" + key + "\" is not a valid key. (Instance not found!)");
-			sb.append("</p>");
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		sb.append("</body></html>");
-		return sb.toString();
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 }
