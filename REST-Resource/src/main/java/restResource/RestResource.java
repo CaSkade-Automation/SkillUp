e3a2ea75b5660a1f06bc24bb9636b96a77a3cd4d
Miguel Vieira <miguel.vieira@hsu-hh.de>
2021-05-27 17:02:31 +0200

    Webservice reacts to skillIri and not UUID. Also skill recognition now via skillIri not UUID. Instead of one method for each possible transition, only one method with variable transition.

--- REST-Resource/src/main/java/restResource/RestResource.java
+++ REST-Resource/src/main/java/restResource/RestResource.java
@@ -2,8 +2,7 @@ package restResource;
 
 import java.io.StringReader;
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.ArrayList;
 
 import javax.json.Json;
 import javax.json.JsonNumber;
@@ -27,11 +26,10 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import skillup.annotations.RestSkillType;
-import skillup.annotations.Skill;
 import skillup.annotations.SkillOutput;
 import skillup.annotations.SkillParameter;
 import statemachine.Isa88StateMachine;
+import states.TransitionName;
 
 @Component(immediate = true, service = RestResource.class)
 @JaxrsResource
@@ -39,10 +37,10 @@ import statemachine.Isa88StateMachine;
 public class RestResource {
 
 	private static Logger logger;
-	private HashMap<UUID, RestSkill> skillDirectory;
+	private ArrayList<RestSkill> skillDirectory;
 
 	public RestResource() {
-		this.skillDirectory = new HashMap<UUID, RestSkill>();
+		this.skillDirectory = new ArrayList<RestSkill>();
 		RestResource.logger = LoggerFactory.getLogger(RestResource.class);
 	}
 
@@ -57,51 +55,19 @@ public class RestResource {
 	}
 
 	public void generateSkill(Object skill, Isa88StateMachine stateMachine) {
-		if (!skill.getClass().isAnnotationPresent(Skill.class)) {
-			logger.info(getClass().getSimpleName()
-					+ ": ERR while generating new skill: Object does not have \"Skill\"-Annotation.");
-			return;
-		}
-		if (!skill.getClass().getAnnotation(Skill.class).type().equals(RestSkillType.class)) {
-			logger.info(getClass().getSimpleName()
-					+ ": ERR while generating new skill: Object's \"Skill\"-Annotation Type is not \"RestSkill\".");
-			return;
-		}
-		RestSkill newSkill = null;
-		do {
-			newSkill = new RestSkill(stateMachine, skill);
-		} while (skillDirectory.containsKey(newSkill.getUUID()));
-		skillDirectory.put(newSkill.getUUID(), newSkill);
-	}
-
-	public RestSkill getRestSkillBySkillObject(Object skillObject) {
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getSkillObject().equals(skillObject)) {
-				return rSkill;
-			}
-		}
-		return null;
+		RestSkill newSkill = new RestSkill(stateMachine, skill);
+		skillDirectory.add(newSkill);
 	}
 
 	public void deleteSkill(Object skill) {
 		logger.info(getClass().getSimpleName() + ": Deleting skill \"" + skill.toString() + "\"...");
 
-		for (UUID key : skillDirectory.keySet()) {
-			if (skillDirectory.get(key).getSkillObject().equals(skill)) {
-				logger.info(getClass().getSimpleName() + ": Found skill in skill directory ("
-						+ skillDirectory.get(key).getSkillIri() + ", " + key.toString() + ")");
-				skillDirectory.remove(key);
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillObject().equals(skill)) {
+				logger.info(
+						getClass().getSimpleName() + ": Found skill in skill directory (" + rSkill.getSkillIri() + ")");
+				skillDirectory.remove(rSkill);
 			}
-		}
-	}
-
-	public boolean isValidUUID(String toCheck) {
-		try {
-			UUID.fromString(toCheck);
-			return true;
-		} catch (Exception e) {
-			logger.error(getClass().getSimpleName() + ": ERR: (" + toCheck + ") is not a valid UUID.");
-			return false;
 		}
 	}
 
@@ -112,9 +78,8 @@ public class RestResource {
 
 		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
 
-		for (RestSkill rSkill : skillDirectory.values()) {
-			objBuilder.add(rSkill.getSkillIri(), Json.createObjectBuilder().add("state", rSkill.getState()).add("uuid",
-					rSkill.getUUID().toString()));
+		for (RestSkill rSkill : skillDirectory) {
+			objBuilder.add(rSkill.getSkillIri(), Json.createObjectBuilder().add("state", rSkill.getState()));
 		}
 
 		String responseString = objBuilder.build().toString();
@@ -123,18 +88,14 @@ public class RestResource {
 	}
 
 	@GET
-	@Path("{uid}")
+	@Path("{skillIri}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response info(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
+	public Response info(@PathParam("skillIri") String skillIri) {
 
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillIri().equals(skillIri)) {
+				JsonObject obj = Json.createObjectBuilder()
+						.add(rSkill.getSkillIri(), Json.createObjectBuilder().add("state", rSkill.getState())).build();
 
 				String responseString = obj.toString();
 				return Response.status(Response.Status.OK).entity(responseString).build();
@@ -146,16 +107,12 @@ public class RestResource {
 	}
 
 	@GET
-	@Path("{uid}/skillOutputs")
+	@Path("{skillIri}/skillOutputs")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSkillOutputs(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
+	public Response getSkillOutputs(@PathParam("skillIri") String skillIri) {
 
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillIri().equals(skillIri)) {
 				// found the correct skill
 				JsonObjectBuilder objBuilder = Json.createObjectBuilder();
 				// check every field of the skillObject for SkillOutput-Annotation
@@ -198,16 +155,12 @@ public class RestResource {
 	}
 
 	@GET
-	@Path("{uid}/skillParameters")
+	@Path("{skillIri}/skillParameters")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSkillParameters(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
+	public Response getSkillParameters(@PathParam("skillIri") String skillIri) {
 
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillIri().equals(skillIri)) {
 				// found the correct skill
 				JsonObjectBuilder objBuilder = Json.createObjectBuilder();
 				// check every field of the skillObject for SkillOutput-Annotation
@@ -259,14 +212,46 @@ public class RestResource {
 	// Booleans
 	// TODO: Better way?
 	@POST
-	@Path("{uid}/skillParameters")
+	@Path("{skillIri}/skillParameters")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response setSkillParameters(@PathParam("uid") String uid, String body) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
+	public Response setSkillParameters(@PathParam("skillIri") String skillIri, String body) {
+
+		JsonObjectBuilder objBuilder = setSkillParameter(skillIri, body);
+
+		String responseString = objBuilder.build().toString();
+		return Response.status(Response.Status.OK).entity(responseString).build();
+	}
+
+	@POST
+	@Path("{skillIri}/{transition}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response transition(@PathParam("skillIri") String skillIri, @PathParam("transition") String transition,
+			String body) {
+
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillIri().equals(skillIri)) {
+
+				// we found the correct skill by skillIri
+				setSkillParameter(skillIri, body);
+				for (TransitionName transitions : TransitionName.values()) {
+					if (transition.equals(transitions.toString())) {
+						rSkill.fireTransition(transitions);
+						break;
+					}
+				}
+				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
+						.add("state", rSkill.getState()).add("skillIri", rSkill.getSkillIri())).build();
+
+				String responseString = obj.toString();
+				return Response.status(Response.Status.OK).entity(responseString).build();
+			}
 		}
 
-		UUID skillKey = UUID.fromString(uid);
+		// no match found!
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+
+	private JsonObjectBuilder setSkillParameter(String skillIri, String body) {
 
 		JsonReader jsonReader = Json.createReader(new StringReader(body));
 		JsonObject jobj = jsonReader.readObject();
@@ -281,8 +266,8 @@ public class RestResource {
 
 		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
 
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(skillKey)) {
+		for (RestSkill rSkill : skillDirectory) {
+			if (rSkill.getSkillIri().equals(skillIri)) {
 				// we found the correct skill by UUID
 				Field[] fields = rSkill.getSkillObject().getClass().getDeclaredFields();
 				for (Field field : fields) {
@@ -307,9 +292,7 @@ public class RestResource {
 				}
 			}
 		}
-
-		String responseString = objBuilder.build().toString();
-		return Response.status(Response.Status.OK).entity(responseString).build();
+		return objBuilder;
 	}
 
 	// Idea: we cast any JsonValue to a String and then convert that String to the
@@ -384,221 +367,5 @@ public class RestResource {
 		}
 		logger.error("End of updateSkillParam -> unknown Datatype?");
 		return false;
-	}
-
-	@POST
-	@Path("{uid}/start")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response start(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.start();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/reset")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response reset(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.reset();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/hold")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response hold(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.hold();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/unhold")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response unhold(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.unhold();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/suspend")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response suspend(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.suspend();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/unsuspend")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response unsuspend(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.unsuspend();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/stop")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response stop(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.stop();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/abort")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response abort(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.abort();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
-	}
-
-	@POST
-	@Path("{uid}/clear")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response clear(@PathParam("uid") String uid) {
-		if (!isValidUUID(uid)) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		UUID key = UUID.fromString(uid);
-
-		for (RestSkill rSkill : skillDirectory.values()) {
-			if (rSkill.getUUID().equals(key)) {
-				rSkill.clear();
-				JsonObject obj = Json.createObjectBuilder().add(rSkill.getSkillIri(), Json.createObjectBuilder()
-						.add("state", rSkill.getState()).add("uuid", rSkill.getUUID().toString())).build();
-
-				String responseString = obj.toString();
-				return Response.status(Response.Status.OK).entity(responseString).build();
-			}
-		}
-
-		// no match found!
-		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 }
