0cdc34699f255185c4a97ffadb9b000d86c90b84
Miguel Vieira <miguel.vieira@hsu-hh.de>
2021-05-27 16:56:59 +0200

    RestSkills now without UUID, because the skillIri should already be unique. Methods that fire certain transitions replaced by one method.

--- REST-Resource/src/main/java/restResource/RestSkill.java
+++ REST-Resource/src/main/java/restResource/RestSkill.java
@@ -1,79 +1,32 @@
 package restResource;
 
-import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import skillup.annotations.Skill;
 import statemachine.Isa88StateMachine;
+import states.TransitionName;
 
 public class RestSkill {
 
 	private static Logger logger = LoggerFactory.getLogger(RestSkill.class);
 
-	private UUID uuid;
 	private Isa88StateMachine stateMachine;
 	private Object skillObject;
 
 	public RestSkill(Isa88StateMachine stateMachine, Object skillObject) {
-		uuid = UUID.randomUUID();
 		this.stateMachine = stateMachine;
 		this.skillObject = skillObject;
-		logger.info("RestSkill \"" + uuid + "\" (skillIri=" + this.getSkillIri() + ") created.");
+		logger.info("RestSkill with skillIri=" + this.getSkillIri() + " created.");
 	}
 
-	public void start() {
-		stateMachine.start();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine start.");
-	}
-
-	public void reset() {
-		stateMachine.reset();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine reset.");
-	}
-
-	public void hold() {
-		stateMachine.hold();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine hold.");
-	}
-
-	public void unhold() {
-		stateMachine.unhold();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine unhold.");
-	}
-
-	public void suspend() {
-		stateMachine.suspend();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine suspend.");
-	}
-
-	public void unsuspend() {
-		stateMachine.unsuspend();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine unsuspend.");
-	}
-
-	public void stop() {
-		stateMachine.stop();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine stop.");
-	}
-
-	public void abort() {
-		stateMachine.abort();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine abort.");
-	}
-
-	public void clear() {
-		stateMachine.clear();
-		logger.info("RestSkill \"" + uuid + "\": StateMachine clear.");
+	public void fireTransition(TransitionName transitionName) {
+		stateMachine.invokeTransition(transitionName);
+		logger.info("RestSkill with skillIri=" + this.getSkillIri() + "\": StateMachine: " + transitionName.toString());
 	}
 
 	public String getState() {
 		return stateMachine.getState().getClass().getSimpleName();
-	}
-
-	public UUID getUUID() {
-		return uuid;
 	}
 
 	public Object getSkillObject() {
